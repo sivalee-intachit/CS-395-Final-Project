8aee07322293aaf1c3e53c540f6a3059750aa3fa
sivalee-intachit <sivalee.intachit@gmail.com>
2024-10-21 00:25:35 -0400

    Added To Do List View

---
+++ 395 Final Project/395 Final Project/Extensions/FontExtension.swift
@@ -0,0 +1,12 @@
+//
+//  FontExtension.swift
+//  395 Final Project
+//
+//  Created by Sivalee Intachit on 10/20/24.
+//
+
+import SwiftUI
+
+extension Font {
+    static let poppins = Font.custom("Poppins-Regular", size: 18)
+}
---
+++ 395 Final Project/395 Final Project/_95_Final_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  _95_Final_ProjectApp.swift
+//  395 Final Project
+//
+//  Created by Justin Vu on 9/16/24.
+//
+
+import SwiftUI
+
+@main
+struct _95_Final_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
